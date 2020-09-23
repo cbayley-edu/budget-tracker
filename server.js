f3629b4487cc8102fb7bab2d2524c47332caf2fe
christina-edu <christina-edu@users.noreply.github.com>
2020-09-22 22:35:01 -0400

    added port var for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3005;
+const PORT = process.env.PORT || 3005;
 
 const app = express();
 
